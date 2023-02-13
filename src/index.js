1bc404d268f4f0eeefd3790ace78b1cb06c2022a
Mikael Moilanen <mikael.moilanen@gmail.com>
2023-02-13 21:07:36 +0200

    Change browserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import CssBaseline from "@mui/material/CssBaseline"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import { ThemeProvider } from "@mui/material/styles"
 import { QueryClient, QueryClientProvider } from "react-query"
 import App from "./App"
